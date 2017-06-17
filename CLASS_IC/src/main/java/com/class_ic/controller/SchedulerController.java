4249679a31f0b2fc94354f3bb64d5d4520475433
kosta-HP\kosta <kkj01331@gmail.com>
2017-06-17 17:55:11 +0900

    attendance dao 폴더만 일단 만들어놈..

--- CLASS_IC/src/main/java/com/class_ic/controller/SchedulerController.java
+++ CLASS_IC/src/main/java/com/class_ic/controller/SchedulerController.java
@@ -10,9 +10,12 @@ import org.springframework.stereotype.Controller;
 public class SchedulerController {
 
 	
+	
 /* @Scheduled(cron="0 42 17 * * *")
  * "초 분 시 일 월 요일"
+ * 일/월/요일에 대해서는 '*' 설정을 잡아 주었으니 "매일 17시 42분이 되면 해당 스케쥴러를 호출하라 라는 뜻 이다."
  * 
+ * @Scheduled(fixedRate=5000) -> 5초마다 실행 해주기
  * */
 	
 	@Scheduled(cron="0 42 17 * * *")
