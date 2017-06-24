15954aaa930b0f913ada4069e5f813724bf6ddba
TEI_JOHN <kteihjohn@gmail.com>
2017-06-24 20:19:29 +0900

    login

--- CLASS_IC/src/main/webapp/resources/assets/js/join_ajax.js
+++ CLASS_IC/src/main/webapp/resources/assets/js/join_ajax.js
@@ -6,17 +6,10 @@ $(function(){
         	var pwd_st = $('#pwd_st').val();
         	var name_st = $('#name_st').val();
         	var phone_st = $('#phone_st').val();
-        	var  photoSrc_st = $('#photoSrc_st').val();
-        	var email_te = $('#email_te').val();
-        	var pwd_te = $('#pwd_te').val();
-        	var name_te = $('#name_te').val();
-        	var phone_te = $('#phone_te').val();
-        	var photoSrc_te = $('#photoSrc_te').val();
+        	var  photoSrc_st = $('#photoSrc_st').val();      	
         	var authority_st  = $('#authority_st').val();
-        	var authority_te = $('#authority_te').val();
-        	
+         	
         	var student_data = {"email":email_st, "pwd": pwd_st, "name":name_st , "phone": phone_st,"photoSrc":photoSrc_st, "authority":authority_st };
-        	var teacher_data = {"email":email_te, "pwd":pwd_te, "name":name_te , "phone":phone_te ,"photoSrc":photoSrc_te, "authority": authority_te };
         	
 
     		$.ajax({
@@ -38,6 +31,15 @@ $(function(){
     	});
     	
     	$('#btn_submit_te').click(function(){
+    		
+    		var email_te = $('#email_te').val();
+        	var pwd_te = $('#pwd_te').val();
+        	var name_te = $('#name_te').val();
+        	var phone_te = $('#phone_te').val();
+        	var photoSrc_te = $('#photoSrc_te').val();
+        	var authority_te = $('#authority_te').val();
+        	var teacher_data = {"email":email_te, "pwd":pwd_te, "name":name_te , "phone":phone_te ,"photoSrc":photoSrc_te, "authority": authority_te };
+        	
     		//alert('클릭');
     		$.ajax({
     			url: "join_te.htm",
@@ -45,11 +47,13 @@ $(function(){
     			data : teacher_data,
     			success : function(data){
     				//console.log(teacher_data);
-    				alert('선생 회원가입 성공');
+    				alert('선생 회원가입 성공' + "\n" + teacher_data);
+    				location.href ="login.htm";
     			},
     			error:function(request, status, error){
     				//console.log(error);
     				alert("code:" + request.status + "\n" + "message:"+ request.responseText + "\n"+ "error: " +error )
+    				location.href ="join.htm";
     			}
     		});
     	});
