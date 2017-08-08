d6e4726e2d34f431f03ccd22d31fa6b0ce9af5d6
Jinhak <wlsgkr91@gmail.com>
2017-08-08 13:32:02 +0900

    진학 주석 추가 및 삭제

--- CLASS_IC/src/main/java/com/class_ic/controller_category/TodoListController.java
+++ CLASS_IC/src/main/java/com/class_ic/controller_category/TodoListController.java
@@ -27,86 +27,68 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 @Controller
-@RequestMapping(value="student")
+@RequestMapping(value = "student")
 public class TodoListController {
-   
-	
-   @Autowired
-   private SqlSession sqlSession;
-   
 
-   /*
-   @description : TodoList에서의 값들을 받아와 DB에 저장
-   */
-   @RequestMapping(value="TodoListInsertOk.htm", method=RequestMethod.GET)
-   public String TodoListInsertOk(HttpServletRequest request) {
-	   HttpSession session=request.getSession();
-	    String email=(String) session.getAttribute("email");
-	   
-	
-			   
-	   System.out.println("todolist컨트롤 탄다.");
-	   
-	   TodoListDAO todolistdao = sqlSession.getMapper(TodoListDAO.class);
-	   TodoListDTO dto = new TodoListDTO();
-	   
-	   String listContent = request.getParameter("listContent");
-	   System.out.println("컨트롤에서 list내용 확인 : " + listContent);
-	   
-	   dto.setTodoList(listContent);
-	   dto.setEmail(email);
-	   
-	   todolistdao.TodoListOk(dto);
-	   
-	   return"student.student_main";
-   }
+	@Autowired
+	private SqlSession sqlSession;
 
-   /*
-   @description : DB에 저장된 일정들을 TodoList에 출력
-   */
-   @RequestMapping(value="todoListselect.htm", method=RequestMethod.GET)
-   public ModelAndView ModelAndView (HttpServletRequest request, HttpServletResponse response) {
-	   HttpSession session=request.getSession();
-	    String email=(String) session.getAttribute("email");
-	   
-	   System.out.println("todolist select 컨트롤러 탄다.");
-	   
-	   TodoListDAO todolistdao = sqlSession.getMapper(TodoListDAO.class);
-	   
-	   ArrayList<TodoListDTO> todolist = todolistdao.TodoList(email);
-	   System.out.println("todolist :" + todolist );
-	   
-	   ModelAndView model = new ModelAndView();
-	   
-	   model.addObject("list",todolist);
-	   model.setViewName("common/todoList");
-	   
-	   return model;
-   }
-   
-   
-   /*
-   @description : TodoList삭제 
-   */
-   @RequestMapping(value="TodoListDelete.htm", method=RequestMethod.GET)
-   public void TodoListDelete(HttpServletRequest request, HttpServletResponse response) throws IOException{
-	   HttpSession session=request.getSession();
-	    String email=(String) session.getAttribute("email");
-	   
-	   System.out.println("todolist 삭제 컨트롤 탄다.");
-	   
-	   TodoListDAO todolistdao = sqlSession.getMapper(TodoListDAO.class);
-       TodoListDTO dto = new TodoListDTO();
-       
-       	int todoNo =Integer.parseInt(request.getParameter("listNo"));
-       	System.out.println("삭제번호 : " + todoNo);
-       	
-       	todolistdao.TodoListDelete(todoNo);
-       	
-       
-   }
-      
-   
-   
-   
+	/*
+	 * @description : TodoList에서의 값들을 받아와 DB에 저장
+	 */
+	@RequestMapping(value = "TodoListInsertOk.htm", method = RequestMethod.GET)
+	public String TodoListInsertOk(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		String email = (String) session.getAttribute("email");
+
+		TodoListDAO todolistdao = sqlSession.getMapper(TodoListDAO.class);
+		TodoListDTO dto = new TodoListDTO();
+
+		String listContent = request.getParameter("listContent");
+
+		dto.setTodoList(listContent);
+		dto.setEmail(email);
+
+		todolistdao.TodoListOk(dto);
+
+		return "student.student_main";
+	}
+
+	/*
+	 * @description : DB에 저장된 일정들을 TodoList에 출력
+	 */
+	@RequestMapping(value = "todoListselect.htm", method = RequestMethod.GET)
+	public ModelAndView ModelAndView(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		String email = (String) session.getAttribute("email");
+
+		TodoListDAO todolistdao = sqlSession.getMapper(TodoListDAO.class);
+
+		ArrayList<TodoListDTO> todolist = todolistdao.TodoList(email);
+
+		ModelAndView model = new ModelAndView();
+
+		model.addObject("list", todolist);
+		model.setViewName("common/todoList");
+
+		return model;
+	}
+
+	/*
+	 * @description : TodoList삭제
+	 */
+	@RequestMapping(value = "TodoListDelete.htm", method = RequestMethod.GET)
+	public void TodoListDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		HttpSession session = request.getSession();
+		String email = (String) session.getAttribute("email");
+
+		TodoListDAO todolistdao = sqlSession.getMapper(TodoListDAO.class);
+		TodoListDTO dto = new TodoListDTO();
+
+		int todoNo = Integer.parseInt(request.getParameter("listNo"));
+
+		todolistdao.TodoListDelete(todoNo);
+
+	}
+
 }
