eb8c988dc65d161aede7e2f4bbe3a95715aaa478
Jinhak <wlsgkr91@gmail.com>
2017-08-08 13:31:50 +0900

    진학 주석 추가 및 삭제

--- CLASS_IC/src/main/java/com/class_ic/controller_category/StudentListController_student.java
+++ CLASS_IC/src/main/java/com/class_ic/controller_category/StudentListController_student.java
@@ -1,3 +1,11 @@
+/*
+* @FileName		:   StudentListController.java
+* 
+* @Project		:	CLASS-IC
+* @Date		    :	2017.06.25
+* @Author		:	이현정
+* @Desc         :   
+*/
 package com.class_ic.controller_category;
 
 import java.io.IOException;
@@ -20,28 +28,21 @@ import com.class_ic.vo.AttandanceDTO;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
-
 @Controller
 @RequestMapping("student")
 public class StudentListController_student {
-	
+
 	@Autowired
 	private StudentListService_student studentListService;
-	
-	@RequestMapping(value="selectStudent.htm", method=RequestMethod.POST)
-	public void studentList(String classCode,HttpServletResponse response) throws IOException {
-		
-		System.out.println("StudentListController classCode: " + classCode);
-		
+
+	@RequestMapping(value = "selectStudent.htm", method = RequestMethod.POST)
+	public void studentList(String classCode, HttpServletResponse response) throws IOException {
+
 		List<AttandanceDTO> studentList = studentListService.selectStudent(classCode);
-		
-		/*model.addAttribute("studentList", studentList);
-		System.out.println("studentList: "+studentList.toString());
-		return "teacher/attendance_check";*/
-		
+
 		JSONArray array = new JSONArray();
-		
-		for(int i=0;i<studentList.size();i++){
+
+		for (int i = 0; i < studentList.size(); i++) {
 			JSONObject obj = new JSONObject();
 			obj.put("photoSrc", studentList.get(i).getPhotoSrc());
 			obj.put("email", studentList.get(i).getEmail());
@@ -50,31 +51,28 @@ public class StudentListController_student {
 			obj.put("inClass", studentList.get(i).getInClass());
 			obj.put("outClass", studentList.get(i).getOutClass());
 			array.add(obj);
-			
-			System.out.println("studentList: "+studentList.toString());
+
 		}
-		
+
 		response.getWriter().println(array);
-		
+
 	}
-	
-	@RequestMapping(value="selectAllInsert.htm", method=RequestMethod.POST)
+
+	@RequestMapping(value = "selectAllInsert.htm", method = RequestMethod.POST)
 	public String insertTeam(HttpServletRequest request) throws IOException {
-		
+
 		String cateCode = request.getParameter("cateCode");
 		String partyName = request.getParameter("partyName");
 		String classCode = request.getParameter("classCode");
 		String name = request.getParameter("selected");
-		
+
 		String[] nameArr = name.split(",");
 		// " , " 구분된 문자열 분해
-		
-		System.out.println("StudentListController 들어왓당");
-		
-		studentListService.insertTeamStudent(cateCode,partyName,classCode,nameArr);
-		
+
+		studentListService.insertTeamStudent(cateCode, partyName, classCode, nameArr);
+
 		return "redirect:makeGroup.htm";
-		
+
 	}
 
 }
