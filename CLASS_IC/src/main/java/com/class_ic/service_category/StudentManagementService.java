a7647bd790af5ef968752ffdf3f302738c47c748
Jinhak <wlsgkr91@gmail.com>
2017-08-08 13:31:55 +0900

    진학 주석 추가 및 삭제

--- CLASS_IC/src/main/java/com/class_ic/service_category/StudentManagementService.java
+++ CLASS_IC/src/main/java/com/class_ic/service_category/StudentManagementService.java
@@ -13,7 +13,6 @@ import com.class_ic.dao.StudentManagementDAO;
 import com.class_ic.vo.StudentGroupDTO;
 import com.class_ic.vo.StudentTableDTO;
 
-
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,60 +24,50 @@ import com.class_ic.vo.StudentTableDTO;
 public class StudentManagementService {
 	@Autowired
 	private SqlSession sqlsession;
-	
-	//학적부 setting
-	public ModelAndView studentTableSetting(ModelAndView mv, StudentTableDTO stable){
-		
+
+	// 학적부 setting
+	public ModelAndView studentTableSetting(ModelAndView mv, StudentTableDTO stable) {
+
 		return mv;
 	}
-	
-	//조편성 setting (과제 카테고리)
-	public String groupSetting(StudentTableDTO stable){
+
+	// 조편성 setting (과제 카테고리)
+	public String groupSetting(StudentTableDTO stable) {
 
 		StudentManagementDAO student_dao = sqlsession.getMapper(StudentManagementDAO.class);
 		int result = student_dao.studentGroupSetting(stable);
-		
+
 		return null;
 	}
-	
-	//조편성 setting 정보 가져와
-	public List<StudentTableDTO> getGroupSettingInfo(){
+
+	// 조편성 setting 정보 가져와
+	public List<StudentTableDTO> getGroupSettingInfo() {
 		StudentManagementDAO student_dao = sqlsession.getMapper(StudentManagementDAO.class);
-		List<StudentTableDTO> groupsetting=  student_dao.getStudentGroupSetting();
-		
+		List<StudentTableDTO> groupsetting = student_dao.getStudentGroupSetting();
+
 		return groupsetting;
 	}
-	
-	//조 나눈거 정보 가져오기 (ajax get)
-		public List<StudentGroupDTO> getstudentGroup(Model m, String classCode, String groupCode){
-			System.out.println(classCode + ": service");
-			StudentManagementDAO student_dao = sqlsession.getMapper(StudentManagementDAO.class);
-	
-			List<StudentGroupDTO> groupList = student_dao.getStudentGroup(classCode, groupCode);
-			List<StudentGroupDTO> groupMemberList = student_dao.getStudentGroupMember(classCode, groupCode);
-			
-			m.addAttribute("memberlist", groupMemberList);
-			
-			return groupList;
-		}
-	
-	//조 나눈거 정보 저장 (ajax post)
-	public void studentGroup(String[] emailArr, String[] groupArr, String groupCode, String classCode, String groupLeaderState){
+
+	// 조 나눈거 정보 가져오기 (ajax get)
+	public List<StudentGroupDTO> getstudentGroup(Model m, String classCode, String groupCode) {
 		StudentManagementDAO student_dao = sqlsession.getMapper(StudentManagementDAO.class);
-		/*
-		 * String[] emailArr= email.split(",");	
-				String[] orderArr = order.split(",");
-				String[] tableArr = groupTable.split(",");
-		 */
-		//int result = student_dao.studentGroupping(emailArr, orderArr, tableArr);
-		
-		
-		StudentGroupDTO sgroup  = new StudentGroupDTO();
-		
-		
-		System.out.println(emailArr.length);
-		System.out.println(groupArr.length);
-		for(int i=0;i<emailArr.length;i++){
+
+		List<StudentGroupDTO> groupList = student_dao.getStudentGroup(classCode, groupCode);
+		List<StudentGroupDTO> groupMemberList = student_dao.getStudentGroupMember(classCode, groupCode);
+
+		m.addAttribute("memberlist", groupMemberList);
+
+		return groupList;
+	}
+
+	// 조 나눈거 정보 저장 (ajax post)
+	public void studentGroup(String[] emailArr, String[] groupArr, String groupCode, String classCode,
+			String groupLeaderState) {
+		StudentManagementDAO student_dao = sqlsession.getMapper(StudentManagementDAO.class);
+
+		StudentGroupDTO sgroup = new StudentGroupDTO();
+
+		for (int i = 0; i < emailArr.length; i++) {
 			sgroup.setEmail(emailArr[i]);
 			sgroup.setGroupName(groupArr[i]);
 			sgroup.setClassCode(classCode);
@@ -86,9 +75,7 @@ public class StudentManagementService {
 			sgroup.setGroupLeaderState(groupLeaderState);
 			student_dao.studentGroupping(sgroup);
 		}
-	
+
 	}
-	
-	
 
 }
